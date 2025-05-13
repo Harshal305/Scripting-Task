bd0002a6af930df70ed74d8a7643ef70f80f7e4b
root <root@ip-172-31-91-255.ec2.internal>
2025-05-13 06:54:35 +0000

    scripting task

---
+++ 5_line.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+FILE="input.txt"
+
+TEMP="temp.txt"
+
+
+line_num=0
+
+
+while IFS= read -r line; do
+  line_num=$((line_num + 1))
+
+  if [ $line_num -lt 5 ]; then
+
+    echo "$line"
+  else
+    if [[ "$line" == *welcome* ]]; then
+
+      echo "${line//give/learning}"
+    else
+
+      echo "$line"
+    fi
+  fi
+done < "$FILE" > "$TEMP"
+
+
+mv "$TEMP" "$FILE"
+ubuntu@ip-172-31-91-255:~/Scripting-Task/scripting$
